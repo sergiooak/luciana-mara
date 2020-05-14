fd797c40d474773ce9744c48776f499cf2e93f5f
Sergio Carvalho <sergiopcn@gmail.com>
2020-05-14 18:22:02 -0300

    initial footer design

--- components/global/Footer.vue
+++ components/global/Footer.vue
@@ -1,13 +1,94 @@
 <template>
-  <footer>
-    footer
+  <footer class="bg-black flex flex-col text-white">
+    <div class="flex-1 flex flex-col justify-center">
+      <div class="container mx-auto flex justify-between">
+        <div class="w-1/2 flex pr-8">
+          <div class="w-1/2 flex flex-col justify-center items-center">
+            <img src="logo/logo-vertical-fundo-escuro.svg" style="height: 100px;" alt="Luciana Mara - Estúdio de Costura">
+            <div class="mt-4">
+              icones sociais
+            </div>
+          </div>
+          <div class="w-1/2">
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco.
+          </div>
+        </div>
+        <div class="w-1/2 flex">
+          <div class="w-1/3">
+            <h6 class="uppercase text-mara">Menu 1</h6>
+            <ul>
+              <li>
+                <nuxt-link to="">Lorem Ipsum</nuxt-link>
+              </li>
+              <li>
+                <nuxt-link to="">Lorem Ipsum</nuxt-link>
+              </li>
+              <li>
+                <nuxt-link to="">Lorem Ipsum</nuxt-link>
+              </li>
+              <li>
+                <nuxt-link to="">Lorem Ipsum</nuxt-link>
+              </li>
+            </ul>
+          </div>
+          <div class="w-1/3">
+            <h6 class="uppercase text-mara">Menu 1</h6>
+            <ul>
+              <li>
+                <nuxt-link to="">Lorem Ipsum</nuxt-link>
+              </li>
+              <li>
+                <nuxt-link to="">Lorem Ipsum</nuxt-link>
+              </li>
+              <li>
+                <nuxt-link to="">Lorem Ipsum</nuxt-link>
+              </li>
+              <li>
+                <nuxt-link to="">Lorem Ipsum</nuxt-link>
+              </li>
+            </ul>
+          </div><div class="w-1/3">
+            <h6 class="uppercase text-mara">Menu 1</h6>
+            <ul>
+              <li>
+                <nuxt-link to="">Lorem Ipsum</nuxt-link>
+              </li>
+              <li>
+                <nuxt-link to="">Lorem Ipsum</nuxt-link>
+              </li>
+              <li>
+                <nuxt-link to="">Lorem Ipsum</nuxt-link>
+              </li>
+              <li>
+                <nuxt-link to="">Lorem Ipsum</nuxt-link>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+      <div class="container mx-auto border-t-2 border-mara mt-8 pt-8">
+        Informações de ssl e formas de pagamento
+      </div>
+    </div>
+    <footer class="bg-mara flex items-center text-center">
+      <div class="container mx-auto font-bold">
+        © 2020 - Luciana Mara Estúdio de Costura - Todos os direitos reservados
+      </div>
+    </footer>
   </footer>
 </template>
 
 <script>
-export default {
-}
+  export default {
+  }
 </script>
 
-<style lang="css" scoped>
+<style lang="scss" scoped>
+  footer{
+    height: 350px;
+
+    footer{
+      height: 50px;
+    }
+  }
 </style>
