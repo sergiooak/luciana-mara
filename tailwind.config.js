30d9e6ced325be521cfc7d553abd88340351050b
Sergio Carvalho <sergiopcn@gmail.com>
2020-05-14 17:35:56 -0300

    added brand colors

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/*
+** TailwindCSS Configuration File
+**
+** Docs: https://tailwindcss.com/docs/configuration
+** Default: https://github.com/tailwindcss/tailwindcss/blob/master/stubs/defaultConfig.stub.js
+*/
+module.exports = {
+  theme: {
+    extend: {
+      colors: {
+        'mara': '#CB4D64',
+        'astronaut': '#003E65',
+        'black': '#1A202C'
+      }
+    }
+  },
+  variants: {},
+  plugins: []
+}
