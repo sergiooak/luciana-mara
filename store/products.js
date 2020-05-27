c6fd2dc994a912f863fc101c0fd35ab5dc956695
Sergio Carvalho <sergiopcn@gmail.com>
2020-05-27 15:06:00 -0300

    hard code env variables, I know its wrong but i'm desparate!!!

--- store/products.js
+++ store/products.js
@@ -1,3 +1,8 @@
+const BASE_URL  = 'https://lucianamara.com.br';
+const API_URL   = 'https://wplucianamara1.websiteseguro.com'
+const TOKEN     = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJodHRwczpcL1wvd3BsdWNpYW5hbWFyYTEud2Vic2l0ZXNlZ3Vyby5jb20iLCJpYXQiOjE1OTAxNjEyMDMsIm5iZiI6MTU5MDE2MTIwMywiZXhwIjoxNTkwNzY2MDAzLCJkYXRhIjp7InVzZXIiOnsiaWQiOiIxIn19fQ.lx4UM48EoSQAaSstT9yKJx5mQD-xrrjF7_mZ58EAix4'
+
+
 export const state = () => ({
   items: {},
   loading: true,
@@ -24,16 +29,14 @@ export const mutations = {
 
 export const actions = {
   retrieve(context, $axios) {
-    let API_URL = process.env.API_URL;
-
     let config = {
       headers: {
-        'Authorization': 'Bearer ' + process.env.TOKEN
+        'Authorization': 'Bearer ' + TOKEN
       }
     }
 
-    console.log(API_URL);
-    console.log(process.env.TOKEN);
+    // console.log(API_URL);
+    // console.log(TOKEN);
 
     var vm = this
     vm.$axios
